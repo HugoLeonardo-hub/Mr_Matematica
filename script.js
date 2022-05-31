403cfbb565f6e757d256ce78aa6ca4c370aba1c9
HugoLeonardo-hub <hugoperes21@gmail.com>
2022-05-31 09:14:42 -0300

    html__css__calc__done
    criação do HTML e CSS da calculadora concluidos

--- script.js
+++ script.js
@@ -33,3 +33,26 @@ function contar(){
      res.innerHTML += `\u{1F3c1}`
     }
 }
+
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('sel-tab')
+
+    if (num.value.length == 0){
+        window.alert ('Por favor digite um número')
+
+    }else{
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c<=10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+
+    }
+
+}
